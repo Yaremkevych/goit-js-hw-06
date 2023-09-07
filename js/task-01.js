780c0fbab2fd09481cdd00b3c51c18eed1af59c0
Yaremkevych <137343042+Yaremkevych@users.noreply.github.com>
2023-09-07 21:39:26 +0200

    remake-task-1
    delete const

--- js/task-01.js
+++ js/task-01.js
@@ -2,8 +2,6 @@ const categoriesItems = document.querySelectorAll("li.item");
 console.log(`Number of categories: ${categoriesItems.length}`);
 
 categoriesItems.forEach((item) => {
-    const title = item.querySelector("h2").textContent;
-    const itemsCount = item.querySelectorAll("li").length;
-    console.log(`Category: ${title}`);
-    console.log(`Elements: ${itemsCount}`);
+    console.log(`Category: ${item.querySelector("h2").textContent}`);
+    console.log(`Elements: ${item.querySelectorAll("li").length}`);
 });
